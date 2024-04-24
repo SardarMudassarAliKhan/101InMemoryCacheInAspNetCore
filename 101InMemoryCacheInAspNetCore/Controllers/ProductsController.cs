3ef60f919ba04befd02ba37bc1717062a4be7276
Sardar Mudassar Ali Khan <55798521+SardarMudassarAliKhan@users.noreply.github.com>
2024-04-24 06:32:13 +0500

    1-InMemory Cache Implementation in Asp.net Core WebAPI

---
+++ 101InMemoryCacheInAspNetCore/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using _101InMemoryCacheInAspNetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace _101InMemoryCacheInAspNetCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IMemoryCache _memoryCache;
+
+        public ProductsController(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> Get(int id)
+        {
+            if (_memoryCache.TryGetValue($"Product_{id}", out Product cachedProduct))
+            {
+                return Ok(cachedProduct);
+            }
+            else
+            {
+                var product = new Product { Id = id, Name = $"Product {id}", Price = 99.99M };
+
+                _memoryCache.Set($"Product_{id}", product, TimeSpan.FromMinutes(10));
+
+                return Ok(product);
+            }
+        }
+    }
+}
---
+++ 101InMemoryCacheInAspNetCore/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace _101InMemoryCacheInAspNetCore.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+}
